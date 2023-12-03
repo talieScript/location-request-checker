49529c8890e50008d5c9ea2d89cf4b91df717c2c
Taliesin <taliesin.bowes@hotmail.co.uk>
2023-12-03 11:03:59 +0000

    update

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-require('dotenv').config();
+const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
+
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Function to fetch data from a table
-async function fetchData() {
+app.use(express.static('public')); // Serve static files from the 'public' directory
+
+// API endpoint to fetch data from the Supabase table
+app.get('/api/data', async (req, res) => {
   try {
     const { data, error } = await supabase.from('your_table_name').select('*');
     if (error) {
       throw error;
     }
-    console.log('Data fetched successfully:', data);
+    res.json(data);
   } catch (error) {
-    console.error('Error fetching data:', error.message);
+    res.status(500).json({ error: error.message });
   }
-}
+});
 
-// Function to insert data into a table
-async function insertData(dataToInsert) {
-  try {
-    const { data, error } = await supabase
-      .from('your_table_name')
-      .insert(dataToInsert);
-    if (error) {
-      throw error;
-    }
-    console.log('Data inserted successfully:', data);
-  } catch (error) {
-    console.error('Error inserting data:', error.message);
-  }
-}
-
-// Example usage
-fetchData(); // Fetch data from the table
-insertData({ column1: 'value1', column2: 'value2' }); // Insert data into the table
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
